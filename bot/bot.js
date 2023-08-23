9f2bfb094c2f538eb21b39c1d5dd091b806d5c74
Qiyas <107604130+qiyascc@users.noreply.github.com>
2023-08-23 19:38:26 +0400

    Fixed

--- bot/bot.js
+++ bot/bot.js
@@ -1,15 +1,20 @@
 const { Telegraf, Markup } = require('telegraf');
 
-const BOT_TOKEN = 'YOUR_BOT_TOKEN';
+const BOT_TOKEN = 'BOT_TOKEN';
 const bot = new Telegraf(BOT_TOKEN);
 
-bot.start((ctx) => {
+
+function botStart(ctx) {
     const name = ctx.from.first_name;
     ctx.reply(`Hello ${name}`, Markup.inlineKeyboard([
-        [Markup.button.callback('Help', 'HELP')],
-        [Markup.button.callback('Support', 'SUPPORT')],
-        [Markup.button.callback('Close', 'CLOSE_START')]
+        [Markup.button.callback('Help', 'HELP'),
+        Markup.button.callback('Support', 'SUPPORT')],
+        [Markup.button.callback('Close', 'CLOSE')]
     ]).resize());
+}
+
+bot.start((ctx) => {
+    botStart(ctx);
 });
 
 bot.command('dev', (ctx) => {
@@ -19,7 +24,10 @@ bot.command('dev', (ctx) => {
 });
 
 bot.action('HELP', (ctx) => {
-    ctx.editMessageText('Help', Markup.inlineKeyboard([
+    ctx.editMessageText("The purpose of this bot is to keep you up to date with certain developments and help you solve your relevant issues." +
+    "\nThe bot will update over time and get better." +
+    "\n\nIf you have a feature or other offer you want to be added, you can notify the developer with the /dev command.", 
+    Markup.inlineKeyboard([
         [Markup.button.callback('Geri', 'BACK_TO_START')],
         [Markup.button.callback('Close', 'CLOSE')]
     ]).resize());
@@ -33,16 +41,12 @@ bot.action('SUPPORT', (ctx) => {
 });
 
 bot.action('BACK_TO_START', (ctx) => {
-    const name = ctx.from.first_name;
-    ctx.editMessageText(`HEllo ${name}`, Markup.inlineKeyboard([
-        [Markup.button.callback('Help', 'HELP')],
-        [Markup.button.callback('Support', 'SUPPORT')],
-        [Markup.button.callback('Close', 'CLOSE')]
-    ]).resize());
+    ctx.deleteMessage();
+    botStart(ctx);
 });
 
 bot.action('CLOSE', (ctx) => {
     ctx.deleteMessage();
-
+});
 
 bot.launch();
