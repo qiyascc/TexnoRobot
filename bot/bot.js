af81d1f915dd9adfb5fca692d35cea938516ab56
Qiyas <107604130+qiyascc@users.noreply.github.com>
2023-09-14 20:15:32 +0400

    Server acc request added

--- bot/bot.js
+++ bot/bot.js
@@ -1,9 +1,10 @@
 const { Telegraf, Markup } = require('telegraf');
-
+const ADMIN_CHAT_ID = 'YOUR_ADMIN_CHAT_ID';
 const BOT_TOKEN = 'BOT_TOKEN';
 const bot = new Telegraf(BOT_TOKEN);
-
-
+const admins = ['id1'];
+let userStates = {};
+const users = JSON.parse(fs.readFileSync('users.json', 'utf8'));
 function botStart(ctx) {
     const name = ctx.from.first_name;
     ctx.reply(`Hello ${name}`, Markup.inlineKeyboard([
@@ -48,5 +49,69 @@ bot.action('BACK_TO_START', (ctx) => {
 bot.action('CLOSE', (ctx) => {
     ctx.deleteMessage();
 });
+
+
+//Server Account Request 
+bot.command('login', (ctx) => {
+    ctx.reply('Please enter your first name.');
+    userStates[ctx.from.id] = 'name';
+});
+
+bot.on('text', async (ctx) => {
+    switch (userStates[ctx.from.id]) {
+        case 'name':
+            if (/^[a-zA-Z]+$/.test(ctx.message.text)) {
+                if (!userStates[ctx.from.id]) userStates[ctx.from.id] = {};
+                userStates[ctx.from.id] = { name: ctx.message.text };
+                await ctx.reply('Now, please enter your last name.');
+                userStates[ctx.from.id] = 'surname';
+            } else {
+                await ctx.reply('That doesn't seem like a valid name.');
+            }
+            break;
+        case 'surname':
+            if (/^[a-zA-Z]+$/.test(ctx.message.text)) {
+                userStates[ctx.from.id] = { surname: ctx.message.text };
+                await ctx.reply('Now, please enter your class/grade.');
+                userStates[ctx.from.id] = 'class';
+            } else {
+                await ctx.reply('That doesn't seem like a valid surname.');
+            }
+            break;
+        case 'class':
+            userStates[ctx.from.id] = { class: ctx.message.text };
+            await ctx.reply('Thank you, we will get back to you soon.');
+
+            const msg = await bot.telegram.sendMessage(
+                ADMIN_CHAT_ID,
+                `New user alert.\n\nID: ${ctx.from.id}\nFirst Name: ${userStates[ctx.from.id].name}\nLast Name: ${userStates[ctx.from.id].surname}\nClass: ${userStates[ctx.from.id].class}`,
+                Markup.inlineKeyboard([
+                    Markup.button.callback('Reject', 'user_reject'),
+                    Markup.button.callback('Approve', 'user_create')
+                ])
+            );
+
+            
+            users[ctx.from.id] = {
+                name: userStates[ctx.from.id].name,
+                surname: userStates[ctx.from.id].surname,
+                class: userStates[ctx.from.id].class
+            };
+            fs.writeFileSync('users.json', JSON.stringify(users, null, 2));
+            break;
+    }
+});
+
+bot.action('user_reject', async (ctx) => {
+    if (admins.includes(String(ctx.from.id))) {
+        const originalMessage = ctx.callbackQuery.message.text;
+        const userId = originalMessage.match(/ID: (\d+)/)[1];
+        bot.telegram.sendMessage(userId, 'Our admin has rejected your request.');
+        ctx.editMessageText(originalMessage + '\n\nThis request was rejected.');
+    } else {
+        ctx.answerCbQuery('Who are you? This button is for my admin.');
+    }
+});
+
 
 bot.launch();
